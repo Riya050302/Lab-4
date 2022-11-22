d62f659d24d20ef71c00527ea42a25607e09c972
zzx04 <106155056+zzx04@users.noreply.github.com>
2022-11-22 15:07:51 +0000

    Update SignEx.sv

--- SignEx.sv
+++ SignEx.sv
@@ -1,8 +1,8 @@
 module SignEx #(
-    logic input [11:0] ImmSrc,
-    logic input [31:0] instr,
-    logic output [31:0] ImmOp
-)
+    input logic [11:0] ImmSrc,
+    input logic [31:0] instr,
+    output logic [31:0] ImmOp
+);
 
 logic [4:0] rd;
 logic [6:0] opcode;
@@ -10,11 +10,11 @@ logic [19:0] imm;
 
 assign opcode = instr[6:0];
 assign rd = instr[11:7];
-assign imm = {8'ImmSrc[11],ImmSrc};
+assign imm = {{8{ImmSrc[11]}},ImmSrc};
 
 if (opcode==7'b0010011) //addi
-    ImmOp <= {imm,rd,opcode};
+    assign ImmOp = {imm,rd,opcode};
 if (opcode==7'b1100011) //bne
-    ImmOp <= {ImmSrc[11],ImmSrc[9:4],instr[24:12],ImmScr[3:0],ImmScr[10],opcode};
+    assign ImmOp = {ImmSrc[11],ImmSrc[9:4],instr[24:12],ImmScr[3:0],ImmScr[10],opcode};
 
 endmodule
