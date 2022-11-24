557a46a01e47bb78e50ca8a9ea88741bc7d9e3d0
bhavyaEE <107200668+bhavyaEE@users.noreply.github.com>
2022-11-24 11:39:22 +0000

    Update PC_Reg.sv

--- PC_Reg.sv
+++ PC_Reg.sv
@@ -1,7 +1,7 @@
 module PC_Reg (
     input logic [31:0]            next_PC,
     input logic                   rst,
-    output logic [31:0]           PC_out
+    output logic [31:0]           PC
 );
 
 always_ff @ (posedge clk, posedge rst)
