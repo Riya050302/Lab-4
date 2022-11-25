f6e3b2a1b83b0902c0cd1d1fcc038ed42eb151c6
zzx04 <106155056+zzx04@users.noreply.github.com>
2022-11-25 10:22:02 +0000

    Update ControlUnit.sv

--- ControlUnit.sv
+++ ControlUnit.sv
@@ -17,23 +17,28 @@ assign opcode = instr[6:0];
 assign funct3 = instr[14:12];
 
 always_latch @(instr) 
-    if (opcode == 7'b0010011 & funct3 == 3'b000) begin
+    if (opcode == 7'b0010011 & funct3 == 3'b000) //add
+        begin
         assign ALUctrl = 3'b000;
         assign ALUsrc = 1'b0;
         assign RegWrite = 1'b1; 
-    end else if (opcode==7'b0010011 & funct3==3'b000 & EQ == 0) begin  
+        end
+    else if (opcode==7'b0010011 & funct3==3'b000 & EQ == 0) //addi
+        begin  
         assign ALUctrl = 3'b000;
         assign ALUsrc = 1'b1;
         assign RegWrite = 1'b1;
         assign ImmSrc = instr[31:20];
-    end else if (opcode == 7'b1100011 & funct3 == 3'b001 & EQ == 0) begin  //bne
+        end
+    else if (opcode == 7'b1100011 & funct3 == 3'b001 & EQ == 0) //bne
+        begin
         assign ImmSrc = instr[31:20];
         assign RegWrite = 1'b0;
         if (EQ==0)
-        assign  PCsrc = 1'b1;
+            assign  PCsrc = 1'b1;
         else
             assign PCsrc = 1'b0; 
-    end
+        end
 
 endmodule   
 
